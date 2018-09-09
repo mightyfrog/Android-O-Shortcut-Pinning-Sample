8ff1317c356883d7dc5d7bb5191ec494f28c3da5
mightyfrog <mightyfrog.gc@gmail.com>
2018-09-09 18:12:09 +0900

    update SDK (28)

--- app/src/main/java/org/mightyfrog/android/shortcutpinningsample/MainActivity.kt
+++ app/src/main/java/org/mightyfrog/android/shortcutpinningsample/MainActivity.kt
@@ -1,6 +1,5 @@
 package org.mightyfrog.android.shortcutpinningsample
 
-import android.app.PendingIntent
 import android.content.Intent
 import android.content.pm.ShortcutInfo
 import android.content.pm.ShortcutManager
@@ -8,6 +7,7 @@ import android.graphics.drawable.Icon
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 /**
  * https://developer.android.com/reference/android/content/pm/ShortcutManager.html
@@ -24,22 +24,26 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener {
             pinShortcut()
         }
+
+        textView.text = intent?.getStringExtra("text")
     }
 
     private fun pinShortcut() {
-        val manager = getSystemService(ShortcutManager::class.java)
-        if (manager != null && manager.isRequestPinShortcutSupported) {
-            val intent = Intent(Intent.ACTION_VIEW) // action required
-            val info = ShortcutInfo.Builder(this, createId())
-                    .setIcon(Icon.createWithResource(this, R.drawable.ic_beach_access))
-                    .setShortLabel(getString(R.string.label))
-                    .setIntent(intent)
-                    .build()
-            manager.requestPinShortcut(info, PendingIntent.getActivity(this, 1, Intent(this, MainActivity::class.java), PendingIntent.FLAG_ONE_SHOT).intentSender)
+        getSystemService(ShortcutManager::class.java)?.also { manager ->
+            if (manager.isRequestPinShortcutSupported) {
+                val intent = Intent(this, MainActivity::class.java).apply {
+                    action = Intent.ACTION_VIEW
+                    putExtra("text", "Hello Shortcut!")
+                }
+                val info = ShortcutInfo.Builder(this, createId())
+                        .setIcon(Icon.createWithResource(this, R.drawable.ic_beach_access))
+                        .setShortLabel(getString(R.string.label))
+                        .setIntent(intent)
+                        .build()
+                manager.requestPinShortcut(info, null)
+            }
         }
     }
 
-    private fun createId(): String {
-        return "" + System.currentTimeMillis()
-    }
+    private fun createId() = System.currentTimeMillis().toString()
 }
